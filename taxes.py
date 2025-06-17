0fedcbb6365fc58b6375b199fb97c5b47124b977
Tim Lucas <tlucas@northweststate.edu>
2025-06-17 17:48:26 -0400

    Assignment Completed

--- taxes.py
+++ taxes.py
@@ -1,13 +1,20 @@
-# Code contains four problems.
+# Tim Lucas
+# 2025-06-17
+
 STATE_TAX_RATE = .051
-COUNTY_TAX_RATE = .025
+#problem = County tax rate incorrectly set to 2.5%; corrected to 2.4%
+COUNTY_TAX_RATE = .024
 
 def main():
     print('This program will calculate your taxes!\n')
 
     sale = float(input('How much is the cost of your purchase? '))
-    calculate_state_tax(sale, STATE_TAX_RATE)
-    county_tax = calculate_county_tax(COUNTY_TAX_RATE, sale)
+    #problem = state_tax not initialized.; corrected by initializing state_tax and
+	#assigned the value returned by calculate state tax
+    state_tax = calculate_state_tax(sale, STATE_TAX_RATE)
+    #problem = parameters in incorrect order. corrected = place in correct order
+    county_tax = calculate_county_tax(sale, COUNTY_TAX_RATE)
+	 
     total_cost = sale + state_tax + county_tax
 
     print('\nSale amount: $', format(sale, '.2f'), sep = '')
@@ -19,6 +26,8 @@ def calculate_state_tax(sales_amount, tax_rate):
 	return(sales_amount * tax_rate)
 	
 def calculate_county_tax(amount, rate):
-	taxes = amount * rate
-
+	#problem = function does not return anything. corrected = added statement to 
+	#return calculated value
+    return(amount * rate)
+    
 main()
